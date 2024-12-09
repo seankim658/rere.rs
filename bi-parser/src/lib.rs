618e6128974226ea4d5b0b19e64757e13634d003
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-12-09 15:45:07 -0500

    Add constants to prelude

--- bi-parser/src/lib.rs
+++ bi-parser/src/lib.rs
@@ -4,7 +4,8 @@ pub mod validator;
 pub mod writer;
 
 pub mod prelude {
-    pub use crate::bi_core::types::BiField;
     pub use crate::bi_core::error::BiError;
+    pub use crate::bi_core::types::BiField;
+    pub use crate::bi_core::{MARKER_BLOB, MARKER_INT, MARKER_SYM, NEWLINE, SPACE};
     pub use crate::parser::reader::BiReader;
 }
