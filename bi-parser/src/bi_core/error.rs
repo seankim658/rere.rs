929d3c2947525f4f222a26b1013f3ccf13136bb9
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-12-09 15:43:37 -0500

    Add BiWriteError

--- bi-parser/src/bi_core/error.rs
+++ bi-parser/src/bi_core/error.rs
@@ -3,9 +3,10 @@
 
 use crate::parser::error::BiParserError;
 use crate::validator::error::BiValidationError;
+use crate::writer::error::BiWriterError;
 use thiserror::Error;
 
-/// Top-level error type combining all possible bi format errors. 
+/// Top-level error type combining all possible bi format errors.
 #[derive(Debug, Error)]
 pub enum BiError {
     /// Low level I/O error.
@@ -15,6 +16,10 @@ pub enum BiError {
     /// Error during parsing of bi format struture.
     #[error("Parse error: {0}")]
     ParseError(#[from] BiParserError),
+
+    /// Error during writing of bi format struture.
+    #[error("Write error: {0}")]
+    WriteError(#[from] BiWriterError),
 
     /// Error during validation of field content.
     #[error("Validation error: {0}")]
